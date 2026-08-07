efbfa0c684bb41dcf2bde4d294ada5d3efb72862
Rab <19318912+rabbielulu@users.noreply.github.com>
2026-08-07 18:47:39 +0800

    Use Hello Mario sounds for Boo interactions

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "boo-night-v9";
+const CACHE_NAME = "boo-night-v10";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css",
-  "./app.js?v=9",
+  "./app.js?v=10",
   "./manifest.webmanifest",
   "./icon.svg",
   "./icon-180.png",
